4f742c528d1de7e8060568383ec234b3e43727f3
CRBroughton <crbroughton@posteo.uk>
2025-09-07 21:30:03 +0100

    a

---
+++ modules/go/playground/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Go demo";
+
+  inputs = {
+    custom-pkgs.url = "path:../../";
+    nixpkgs.follows = "custom-pkgs/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      custom-pkgs,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        cpkgs = custom-pkgs.modules.${system};
+        cpkgsShell = custom-pkgs.lib.${system}.mkShell;
+      in
+      {
+        devShells.default = cpkgsShell {
+          buildInputs = with cpkgs; [
+            go
+          ];
+        };
+      }
+    );
+}
